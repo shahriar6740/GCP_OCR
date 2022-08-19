c69b20b9ec2698394b6d0ad82df5ed0e2d3f86aa
shahriar6740 <hasan.shahriar@ideascale.com>
2022-08-19 16:15:54 +0600

    fixed values.

--- OCR_API_project/OCR_API/settings.py
+++ OCR_API_project/OCR_API/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 import os
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,12 +21,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-lqglaf+xrxamtiagam0k@!lb=gmnk%36=r7d@het9w%@!x334='
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG')
 
-ALLOWED_HOSTS = ['127.0.0.1', '0.0.0.0']
+ALLOWED_HOSTS = config('ALLOWED_HOSTS')
 
 
 # Application definition
